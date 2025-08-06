3cea3d54d4e4c005495ded3737cc41d440693e62
sulimanshams <sulimanshamsn191@gmail.com>
2025-08-06 17:27:51 +0430

    new project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        background: "#343964",
+        minHeight:"100vw",
+        width:"100vw"
+      }}
+    ></div>
+  );
+}
+
+export default App;
